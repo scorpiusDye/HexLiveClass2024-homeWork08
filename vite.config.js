86e66e06e47b25d56f05be126437f1b9d2838d25
scorpiusDye <166988307+scorpiusDye@users.noreply.github.com>
2024-08-26 23:32:19 +0800

    環境建置

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/HexLiveClass2024-homeWork06/',
+  base: '/HexLiveClass2024-homeWork08/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
